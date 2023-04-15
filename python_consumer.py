1f5739bb48191a3584111977c565218655263c22
omveda <vedadashan@gmail.com>
2023-04-15 15:38:50 -0500

    independent consumer

---
+++ python_consumer.py
@@ -0,0 +1,27 @@
+# This code works like a charm for a developer user who is in their own namespace
+#
+from confluent_kafka import Consumer, KafkaError
+
+conf = {
+    'bootstrap.servers': 'crdb-cluster-kafka-bootstrap.crdb-kafka.svc.cluster.local',
+    'group.id': 'my-group-id',
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(conf)
+
+consumer.subscribe(['user7-table-changes'])
+
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            print('End of partition reached')
+        else:
+            print('Error while consuming message: {}'.format(msg.error()))
+    else:
+        print('Received message: {}'.format(msg.value().decode('utf-8')))
+
